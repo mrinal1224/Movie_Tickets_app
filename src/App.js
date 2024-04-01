6c700c46139f0731ce2f4a0cc395733ae146254b
mrinal1224 <iammrinalkc@gmail.com>
2024-04-01 22:02:05 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
 
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 function App() {
   return (
     <div>
-       <h1>Movie ticket app</h1>
+        <BrowserRouter>
+        
+         <Routes>
+      
+         <Route path='/' element={<Home/>}/>
+         <Route path='/login' element={<Login/>}/>
+         <Route path='/register' element={<Register/>}/>
+       
+       
+
+
+         </Routes>
+        
+        </BrowserRouter>
     </div>
   );
 }
